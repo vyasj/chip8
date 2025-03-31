1bab8d6e80cdbc4df3cfb084dc30666f5de3e1af
vyasj <jav_001@hotmail.com>
2025-03-30 23:01:17 -0400

    i have been through war.

--- src/disp.rs
+++ src/disp.rs
@@ -1,7 +1,7 @@
 pub struct Display {
 	pub height: u8,
 	pub width: u8,
-	pub screen: Vec<u16>
+	pub screen: Vec<u8>
 }
 
 impl Display {
@@ -9,17 +9,37 @@ impl Display {
 		let ret = Display {
 			height: 32,
 			width: 64,
-			screen: vec![0; 32*64]
+			screen: vec![0; 64*32]
 		};
 
 		ret
 	}
 
-	pub fn update(&mut self, vals: Vec<u16>, posx: u8, posy: u8) {
-		()
+	pub fn update(&mut self) {
+		let mut idx: usize = 0;
+		while self.screen[idx] == 0 {
+			idx = idx + 1;
+
+			if idx >= self.screen.len() {
+				idx = 0;
+				self.screen[idx] = 1;
+			}
+		}
+		self.screen[idx] = 0;
+		self.screen[idx+1] = 1;
 	}
 
-	pub fn render(self) {
-		()
+	pub fn draw(&self, frame: &mut [u8]) {
+		for (idx, pixel) in frame.chunks_exact_mut(4).enumerate() {
+			let the_shit = self.screen[idx] == 1;
+
+			let rgba = if the_shit {
+				[0x00, 0x00, 0x00, 0xFF]
+			} else {
+				[0xFF, 0xFF, 0xFF, 0xFF]
+			};
+
+			pixel.copy_from_slice(&rgba);
+		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-extern crate sdl2;
-
-use sdl2::{Sdl, VideoSubsystem, EventPump};
-use sdl2::event::Event;
-use sdl2::render::WindowCanvas;
-use sdl2::video::Window;
-use sdl2::pixels::Color;
-use sdl2::keyboard::Keycode;
+use muda::{Menu, MenuEvent, Submenu};
+use pixels::{Error, Pixels, SurfaceTexture};
+use tao::dpi::LogicalSize;
+use tao::event::{Event, KeyEvent, WindowEvent};
+use tao::event_loop::{ControlFlow, EventLoop};
+use tao::keyboard::KeyCode;
+use tao::window::WindowBuilder;
+use tao::platform::unix::WindowExtUnix as _;
 
 use chip8::mem::Memory;
 use chip8::disp::Display;
 use chip8::reg::Registers;
 
 use std::env;
-use std::time::Duration;
+use std::sync::Arc;
 
-fn main() {
+fn main() -> Result<(), Error> {
     println!("Hello, CHIP-8!");
     let filename: String = env::args()
         .nth(1)
@@ -32,42 +32,88 @@ fn main() {
     println!("Initializing registers...");
     let mut registers: Registers = Registers::init();
 
-    println!("Initializing display...");
-    let mut display: Display = Display::init();
+    println!("Initializing display cache...");
+    let mut display = Display::init();
 
     println!("Rendering display window...");
-    let sdl_context: Sdl = sdl2::init().unwrap();
-    let video_subsystem: VideoSubsystem = sdl_context.video().unwrap();
+    let event_loop = EventLoop::new();
+    let window = {
+        let size = LogicalSize::new(display.width as f64, display.height as f64);
+        let window = WindowBuilder::new()
+            .with_title("CHIP-8 bullshit")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap();
+        Arc::new(window)
+    };
 
-    let window: Window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, Arc::clone(&window));
+        Pixels::new(display.width as u32, display.height as u32, surface_texture)?
+    };
+
+    let menu = Menu::new();
+
+    use tao::platform::unix::WindowExtUnix as _;
+    let file_menu = Submenu::new("File", true);
+    menu.append(&file_menu).unwrap();
+    file_menu
+        .append(&muda::MenuItem::with_id("quit", "Quit", true, None))
+        .unwrap();
+    menu.init_for_gtk_window(window.gtk_window(), window.default_vbox())
         .unwrap();
 
-    let mut canvas: WindowCanvas = window.into_canvas().build().unwrap();
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    event:
+                        KeyEvent {
+                            physical_key: KeyCode::Escape,
+                            ..
+                        },
+                    ..
+                } => {
+                    *control_flow = ControlFlow::Exit;
+                }
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+                WindowEvent::Resized(size) => {
+                    if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                        println!("{}", err);
+                        *control_flow = ControlFlow::Exit;
+                    }
+                }
 
-    let mut event_pump: EventPump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
-                    break 'running
-                },
                 _ => {}
+            },
+
+            Event::MainEventsCleared => {
+                display.update();
+                window.request_redraw();
+            }
+
+            Event::RedrawRequested(_) => {
+                display.draw(pixels.frame_mut());
+                if let Err(err) = pixels.render() {
+                    println!("{}", err);
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+
+            _ => {
+                if let Ok(event) = MenuEvent::receiver().try_recv() {
+                    if event.id.0 == "quit" {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                }
             }
         }
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
+    });
+
+    Ok(())
 }
 
 
@@ -112,5 +158,12 @@ mod tests {
         memory.load_rom(filename);
 
         assert!(memory.ram[0x200] != 0);
+    }
+
+    #[test]
+    fn test_display() {
+        let display = Display::init();
+
+        //
     }
 }
